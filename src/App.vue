12192965886efee983f38b053dc6a33d7ac2f663
Linn-htet123 <linnhtet6102005@gmail.com>
2023-11-04 14:24:58 +0630

    clean

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <main>
+    <header>
+      <header>
+        <img src="https://pinia.vuejs.org/logo.svg" alt="pinia logo">
+        <h1>Pinia Tasks</h1>
+      </header>
+    </header>
+  </main>
+</template>
+
+<script>
+export default {
+
+  setup() {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
